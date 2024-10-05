e5775086d488497e9ce5746a1d06e829ecd33fd9
Pysio <qq593277393@outlook.com>
2024-10-05 18:13:54 +0800

    style: 页脚格式修改

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -26,7 +26,7 @@ export default hopeTheme({
   sidebar,
 
   // 页脚
-  footer: `Pysio's Home / 蜀ICP备2023021309号-1 / 构建与: <a href="https://github.com/pysio2007/Vue-blog/commit/${process.env.VUE_APP_GIT_HASH}" target="_blank" style="color: inherit; text-decoration: underline;"> ${process.env.VUE_APP_GIT_HASH}</a>`,
+  footer: `Pysio's Home / 蜀ICP备2023021309号-1 / <a href="https://github.com/pysio2007/Vue-blog/commit/${process.env.VUE_APP_GIT_HASH}" target="_blank" style="color: inherit; text-decoration: underline;">${process.env.VUE_APP_GIT_HASH}</a>`,
   displayFooter: true,
 
   // 博客相关
