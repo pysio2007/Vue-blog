208ab620953ba83f27d60c95b2e601cf2d780350
Pysio <qq593277393@outlook.com>
2024-10-05 17:21:33 +0800

    feat: 使用Prismjs 高亮代码

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -59,7 +59,14 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
+
+    //代码高亮
+    prismjs: {
+      theme: "one-light",
+      lineNumbers: false,
+    },
     
+    //RSS
     feed: {
       rss: true,
       channel: {ttl : 60,}
