cad25132b4e81681f353139e89373f4d9efc7082
Pysio <qq593277393@outlook.com>
2024-10-07 02:25:31 +0800

    feat: 添加GitHub Pull Requests close #1

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,8 +1,10 @@
 import { defineClientConfig } from "vuepress/client";
 import GithubIssues from "./components/GithubIssues.vue";
+import GitHubPRList from './components/GitHubPRList.vue';
 
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
+    app.component('GitHubPRList', GitHubPRList);
     app.component("GithubIssues", GithubIssues);
   },
 });
---
+++ src/other/github-prs.md
@@ -0,0 +1,7 @@
+---
+title: GitHub Pull Requests
+icon: fa-regular fa-code-pull-request fa-lg
+article: false
+---
+
+<GitHubPRList />
