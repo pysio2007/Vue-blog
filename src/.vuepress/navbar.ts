1c63807f4299c549884a23deb48c18a0a246197a
NekoXii Strel1zia <2414500123@qq.com>
2024-10-20 12:32:26 +0800

    feat: 顶部栏增加小曦

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -15,6 +15,7 @@ export default navbar([
         children: [
           { text: "日常", icon: "fa-regular fa-book", link: "daily/" },
           { text: "开发", icon: "fa-regular fa-code", link: "develop/" },
+          { text: "小曦", icon: "fa-solid fa-heart", link: "NekoXii/" },
         ],
       },      
     ],
