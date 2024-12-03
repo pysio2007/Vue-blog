30e91d41a641248109fb0d8705f61a6aae9f9443
Pysio <qq593277393@outlook.com>
2024-12-03 19:23:17 +0800

    feat: 随机图片调用API增加获取中

--- src/.vuepress/components/Randompicturecount.vue
+++ src/.vuepress/components/Randompicturecount.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="hint-container tip">
-      <p class="hint-container-title">截至 {{ currentTime }}，API总请求次数是：{{ count }}</p>
-    </div>
-  </template>
+  <div class="hint-container tip">
+    <p class="hint-container-title">
+      截至 {{ currentTime }}，API总请求次数是：
+      <span v-if="loading">获取中...</span>
+      <span v-else>{{ count }}</span>
+    </p>
+  </div>
+</template>
   
   <script>
   export default {
     data() {
       return {
         count: 0,
-        currentTime: ''
+        currentTime: '',
+        loading: false
       };
     },
     created() {
@@ -18,12 +23,15 @@
     },
     methods: {
       async fetchData() {
+        this.loading = true;
         try {
           const response = await fetch('https://blogapi.pysio.online/random_image_count');
           const data = await response.json();
           this.count = data.count;
         } catch (error) {
           console.error('Error fetching data:', error);
+        }finally {
+          this.loading = false;
         }
       },
       updateTime() {
